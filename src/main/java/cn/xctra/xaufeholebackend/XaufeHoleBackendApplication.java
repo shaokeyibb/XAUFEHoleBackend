6f88f35b2647218f874c478364bae2bed93d4c61
HikariLan <shaokeyibb@126.com>
2022-10-04 04:22:36 +0800

    initial commit

---
+++ src/main/java/cn/xctra/xaufeholebackend/XaufeHoleBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.xctra.xaufeholebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaufeHoleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XaufeHoleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xctra/xaufeholebackend/XaufeHoleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xctra.xaufeholebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XaufeHoleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
